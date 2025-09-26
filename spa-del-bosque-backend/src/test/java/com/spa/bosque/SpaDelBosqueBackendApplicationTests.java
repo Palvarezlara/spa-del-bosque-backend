bd3acf43e51616ef4068780353fe91af7144e6f2
PalvarezLara <pame.alvarez@duocuc.cl>
2025-09-26 19:14:25 -0300

    chore: inicializar proyecto SPRING BOOT/Estructura

---
+++ spa-del-bosque-backend/src/test/java/com/spa/bosque/SpaDelBosqueBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spa.bosque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaDelBosqueBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
