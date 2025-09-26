c768230beaba9282c764061be443e5f893c01a8c
PalvarezLara <pame.alvarez@duocuc.cl>
2025-09-26 19:37:54 -0300

    feat: se crea archivos repository Reserva y Servicio con su logica

--- spa-del-bosque-backend/src/main/java/com/spa/bosque/repository/ServicioRepository.java
+++ spa-del-bosque-backend/src/main/java/com/spa/bosque/repository/ServicioRepository.java
@@ -1,5 +1,13 @@
 package com.spa.bosque.repository;
 
-public class ServicioRepository {
-    
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spa.bosque.entity.Servicio;
+
+public interface ServicioRepository extends JpaRepository<Servicio, Long> {
+  Optional<Servicio> findBySku(String sku);
+  List<Servicio> findByCategoria(String categoria);
 }
